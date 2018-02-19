d9b316ffa461cef0771cc70506be253426c23398
CodingCarlos <carloshdmn@gmail.com>
2018-02-19 20:06:20 +0100

    add fist test example

--- recursos/testing-ejemplo1/tests.js
+++ recursos/testing-ejemplo1/tests.js
@@ -1,3 +1,11 @@
 
 // Aqui los tests
 
+QUnit.module("Funcion sumar", function () {
+
+	QUnit.test("sumar(1, 1)", function( assert ) {
+		var resultado = sumar(1, 1);
+		assert.equal(resultado, 2, "1 + 1 = 2" );
+	});
+
+});
